e23901a07a35f3cba65afdb0c105a69308c28ff3
manthehunted <man.the.hunted@gmail.com>
2025-10-07 15:03:45 -0400

    chore save my configs

---
+++ nvim/lua/custom/plugins/dadbod.lua
@@ -0,0 +1,15 @@
+return {
+  'tpope/vim-dadbod',
+  'kristijanhusak/vim-dadbod-completion',
+  'kristijanhusak/vim-dadbod-ui',
+  cmd = {
+    'DBUI',
+    'DBUIToggle',
+    'DBUIAddConnection',
+    'DBUIFindBuffer',
+  },
+  init = function()
+    -- Your DBUI configuration
+    vim.g.db_ui_use_nerd_fonts = 1
+  end,
+}
---
+++ nvim/lua/kickstart/plugins/.evalblock.lua
@@ -0,0 +1,6 @@
+-- https://github.com/LazyVim/LazyVim/discussions/2202
+return {
+  'evalblock',
+  name = 'evalblock',
+  dev = { path = '/Users/sryu/.config/nvim/lua/custom/' },
+}
